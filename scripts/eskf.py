566199eefb5f16c02131de41b300cbd2b2699b1f
Christopher Strøm <detoxte@gmail.com>
2022-04-24 13:48:16 +0200

    fix measurement update bugs and ros interface
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- scripts/eskf.py
+++ scripts/eskf.py
@@ -38,7 +38,7 @@ class ESKF:
         self.nom_state = np.zeros((self.N_nom_state , 1)) #[pos, vel, orientation (quat), bias_accel, bias_ang_vel]
         self.err_state = np.zeros((self.N_err_state, 1)) #[pos, vel, orientation (eulr), bias_accel, bias_ang_vel] (errors)
 
-        self.P = p * np.eye(self.N_nom_state)
+        self.error_state_covariance = p * np.eye(self.N_err_state)
 
         # Inverse time constants for the bias processes
         self.p_bias_accel = p_ba
@@ -51,20 +51,32 @@ class ESKF:
         rospy.Subscriber("/imu/data_raw", Imu, self.update, queue_size=100)
         rospy.Subscriber("/dvl/data_raw", Twist, self.correct, queue_size=3)
 
+        # Avoid dt in propagate growing too high
+        self.first_imu_msg = True
+        self.last_time = time.time()
 
-        # Wait for imu data to publish here, to avoid dt in propagate growing too high
-        self.last_time = time.time
+
 
     def n(self):
         # Sample 12-dim zero mean process noise vector
-        return np.random.multivariate_normal(np.zeros(self.N_noise_terms), self.process_noise_covariance)
+        return np.random.multivariate_normal(np.zeros(self.N_noise_terms), self.process_noise_covariance).reshape((self.N_noise_terms, 1))
 
     def w(self):
         # Sample 3-dim zero mean measurement noise vector
-        return np.random.multivariate_normal(np.zeros(self.N_meas_terms), self.measurement_noise_covariance)
+        return np.random.multivariate_normal(np.zeros(self.N_meas_terms), self.measurement_noise_covariance).reshape((self.N_meas_terms, 1))
+
+    def Hx(self):
+        """
+        Measurement model. This is a very simple model since the DVL gets us velocity measurements in body, in relation to world.
+        """
+        H = np.zeros((3, self.N_nom_state))
+        H[:3, 3:6] = np.eye(3)
+        return H
+
+
 
     def predict_measurement(self):
-        z_pred = self.H @ self.nom_state + self.w()
+        z_pred = self.Hx() @ self.nom_state + self.w()
         return z_pred
 
     def compose_state(self):
@@ -92,63 +104,66 @@ class ESKF:
         return x
 
 
-
     def update(self, imu_data):
-        measured_accel = imu_data.linear_acceleration
-        measured_ang_vel = imu_data.angular_velocity
 
-        # TODO: transfer imu data to numpy arrays
+        if self.first_imu_msg:
+            self.last_time = time.time()
+            self.first_imu_msg = False
 
-        T = time.time - self.last_time
+        a = imu_data.linear_acceleration
+        v = imu_data.angular_velocity
+
+        measured_accel = np.array([a.x, a.y, a.z])
+        measured_ang_vel = np.array([v.x, v.y, v.z])
+
+        T = time.time() - self.last_time
 
         A, G = self.discretize(measured_accel, measured_ang_vel, T)
 
         err_state_dot = A @ self.err_state + G @ self.n()
 
-
         self.err_state +=  T * err_state_dot # simple euler integration
-        self.last_time = time.time
+        self.last_time = time.time()
+
+        rospy.loginfo(self.err_state)
 
 
     def correct(self, dvl_data):
-        velocity = dvl_data.linear
-        # TODO: transfer dvl data to numpy arrays
-
-
-        z = self.H @ velocity       
+        v = dvl_data.linear
+        measured_vel = np.array([v.x, v.y, v.z]).reshape((3, 1))
 
         # EKF update
-        W = self.P @ self.H.T @ la.inv(self.H@self.P@self.H.T + self.process_noise_covariance)
-        self.err_state = W @ (z - self.h())
-        self.P = (np.eye(self.N_nom_state) - W @ self.H()) @ self.P
+        H = self.measurement_jacobian()
+        P = self.error_state_covariance 
+
+        W = P @ H.T @ la.inv(H@P@H.T + self.measurement_noise_covariance)
+        self.err_state = W @ (measured_vel - self.predict_measurement())
+        P = (np.eye(self.N_err_state) - W @ H) @ P
         self.nom_state = self.compose_state()
 
         # Reset covariance
         G = np.eye(self.N_err_state)
         G[6:9, 6:9] = np.eye(3) - self.S(0.5 * self.err_state[6:9])
-        self.P = G @ self.P @ G.T
+        self.error_state_covariance = G @ P @ G.T
 
         # Reset error state
         self.err_state = np.zeros((self.N_err_state, 1))
 
 
-    def H(self):
-        # Measurement model for velocity inputs
-        # Naive: only considers 2D movement
 
-        HX = np.zeros((3, self.N_nom_state))
-        HX[:3, 3:6] = np.eye(3)
+    def measurement_jacobian(self):
+        # "H" in the literature
+
+        q = self.nom_state[6:10].T[0]
+        eta = q[:1]
+        eps = q[1:]
+
+        Q_dtheta = 0.5 * np.r_[-eps.reshape((1, 3)), eta*np.eye(3) + self.S(eps)]
+        X_dx = la.block_diag(np.eye(6), Q_dtheta, np.eye(6))
         
-        #q = self.nom_state[6:10].T[0]
-        #eta = q[:1]
-        #eps = q[1:]
+        H = self.Hx() @ X_dx
 
-        #Q_dtheta = 0.5 * np.r_[-eps.reshape((1, 3)), eta*np.eye(3) + self.S(eps)]
-        #X_dx = la.block_diag(np.eye(6), Q_dtheta, np.eye(6))
-        
-        #H = HX @ X_dx
-
-        return HX
+        return H.astype(np.float64)
 
     def R(self, q):
         eta = q[:1]
@@ -158,7 +173,7 @@ class ESKF:
         return R
 
     def S(self, v):
-        S = np.array([0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0], dtype=np.float64).reshape((3, 3))
+        S = np.array([0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0], dtype=object).reshape((3, 3))
 
         return S
 
@@ -171,9 +186,9 @@ class ESKF:
         A = np.zeros((self.N_err_state, self.N_err_state))
 
         A[:3, 3:6] = np.eye(3)
-        A[3:6, 6:9] = -self.R(q) @ self.S(measured_accel - bias_accel)
+        A[3:6, 6:9] = -self.R(q) @ self.S(measured_accel.reshape((3, 1)) - bias_accel)
         A[3:6, 9:12] = -self.R(q)
-        A[6:9, 6:9] = -self.S(measured_ang_vel - bias_ang_vel)
+        A[6:9, 6:9] = -self.S(measured_ang_vel.reshape((3, 1)) - bias_ang_vel)
         A[6:9, 12:15] = -np.eye(3)
         A[9:12, 9:12] = -self.p_bias_accel * np.eye(3)
         A[12:15, 12:15] = -self.p_bias_ang_vel * np.eye(3)
@@ -205,7 +220,7 @@ if __name__ == "__main__":
 
 
     try:
-        eskf = ESKF(p=0.001, p_ba=0.001, p_bv=0.001, n=0.001, r=0.001)
+        eskf = ESKF(p=1e-8, p_ba=1e-8, p_bv=1e-6, n=0.001, r=0.001)
         rospy.spin()
 
     except rospy.ROSInterruptException:
